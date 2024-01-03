7a82ff736054e0063af43f20dd9517794f92d573
kesavan06 <kesavan.ce2023@gmail.com>
2024-01-03 23:19:15 +0700

    Profile window

---
+++ popup_logic.js
@@ -0,0 +1,10 @@
+document.getElementById("Profile-image").addEventListener('click',function(){
+    document.getElementById('profilePopup').style.display="block";
+})
+
+document.getElementById("close-popup").addEventListener('click',function(){
+    document.getElementById('profilePopup').style.display="none";
+})
+
+
+
